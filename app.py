b2b1a70653b6095c2e3aa411e35c4ea3090bc6ad
FrankPonce <108038901+FrankPonce@users.noreply.github.com>
2024-09-27 18:50:52 -0400

    render v9

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def stream_kubernetes_data_route():
     return stream_kubernetes_data()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get('PORT', 5000))  # Fallback to 5000 if PORT not set
+    app.run(debug=True, host='0.0.0.0', port=port)
